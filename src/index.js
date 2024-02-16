9df147b22708ff585a1da817d36630c51bc1d33e
Ishitaa Dhingra <ishitaadhingra@gmail.com>
2024-02-16 22:47:34 +0530

    connecting react with redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import store from "./store";
@@ -7,7 +8,9 @@ import store from "./store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
