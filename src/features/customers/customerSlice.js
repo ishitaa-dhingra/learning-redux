3fe1ba50e99c6e2601e378558d549f4bf14eb578
Ishitaa Dhingra <ishitaadhingra@gmail.com>
2024-02-23 02:18:15 +0530

    adding redux-devtools-extension

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -27,5 +27,5 @@ export function createCustomer(fullName, nationalID) {
 }
 
 export function updateName(fullName) {
-  return { type: "account/updateName", payload: fullName };
+  return { type: "customer/updateName", payload: fullName };
 }
