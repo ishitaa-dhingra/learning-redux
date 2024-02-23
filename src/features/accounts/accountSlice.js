eebc591affa0f75d30f6c9a1791b1a618de7cdad
Ishitaa Dhingra <ishitaadhingra@gmail.com>
2024-02-24 03:51:11 +0530

    creating accountSlice with RTK

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -1,9 +1,66 @@
-const initialStateAccount = {
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
   isLoading: false,
 };
+
+const accountSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    deposit(state, action) {
+      state.balance += action.payload;
+      state.isLoading = false;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestloan: {
+      prepare(amount, purpose) {
+        return {
+          payload: { amount, purpose },
+        };
+      },
+
+      reducer(state, action) {
+        if (state.loan > 0) return;
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance += action.payload.amount;
+      },
+    },
+    payloan(state) {
+      state.balance -= state.loan;
+      state.loan = 0;
+      state.loanPurpose = "";
+    },
+
+    convertingCurrency(state) {
+      state.isLoading = true;
+    },
+  },
+});
+console.log(accountSlice);
+export const { withdraw, requestloan, payloan } = accountSlice.actions;
+
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}0&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+
+    dispatch({ type: "account/deposit", payload: converted });
+  };
+}
+export default accountSlice.reducer;
+/*
 export default function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
@@ -70,3 +127,4 @@ export function requestloan(amount, purpose) {
 export function payloan() {
   return { type: "account/payloan" };
 }
+*/
